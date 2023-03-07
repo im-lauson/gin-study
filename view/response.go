60b241ddb41abe402d044d423b5ac4b5fe46f602
im-lauson <im.lauson@gmail.com>
2023-03-07 11:12:30 +0800

    file响应

--- view/response.go
+++ view/response.go
@@ -15,7 +15,7 @@ func _string(c *gin.Context) {
 
 /*
 *
-响应json数据
+响应json结构体是最常用的（重点）
 */
 func _json(c *gin.Context) {
 	//定义一个结构体
@@ -28,7 +28,37 @@ func _json(c *gin.Context) {
 	//实例化user信息
 	user := UserInfo{"songsong", 23, "123456"}
 	c.JSON(200, user)
+
+	//json响应map
+	//userMap := map[string]string{
+	//	"user_name": "liusong",
+	//	"age":       "73",
+	//}
+	//c.JSON(200, userMap)
+
+	//直接响应json
+	//c.JSON(http.StatusOK, gin.H{"username": "liusong", "age": "345"})
 }
+
+/*
+*
+响应xml
+*/
+func _xml(x *gin.Context) {
+	type UserInfo struct {
+		//属性包括username、age、password
+		Username string `json:"user_name"`
+		Age      int    `json:"age"`
+		Password string `json:"password"` //  “-”代表不去渲染数据，不去进行json序列化
+	}
+	user := UserInfo{"songsong", 23, "123456"}
+
+	x.XML(http.StatusOK, user)
+}
+
+/**
+响应xml、html、yaml
+*/
 
 func main() {
 	//创建一个路由
@@ -36,6 +66,7 @@ func main() {
 	//绑定路由规则和路由函数，访问index的路由，交由对应的函数去处理
 	Route.GET("/string", _string)
 	Route.GET("/json", _json)
+	Route.GET("/xml", _xml)
 	//启动监听，gin会把web服务启动在8080端口
 	Route.Run(":8080")
 }
